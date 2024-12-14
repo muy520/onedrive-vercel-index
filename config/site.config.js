f71a1c0c204fd670f61ff6224a2e073d1276068e
牧羊 <42006467+muy520@users.noreply.github.com>
2024-12-14 14:24:13 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/movies/私密','/Galgame', '/movies/网剧', '/movies/DB', '/movies/iCloud', '/movies/rss'],
+  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/movies/私密','/Galgame', '/movies/网剧', '/movies/DB', '/movies/iCloud', '/movies/rss','/Books/摄影/r18（password：i want to see r18）'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:i@ilol.top',
